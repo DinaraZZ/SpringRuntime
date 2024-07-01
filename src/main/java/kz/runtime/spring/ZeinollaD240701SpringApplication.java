2192630b89e19eda51255c320ad3fc070055f400
DinaraZZ <dinara.rakhymbekova@gmail.com>
2024-07-01 12:30:48 +0500

    Spring first commit

---
+++ src/main/java/kz/runtime/spring/ZeinollaD240701SpringApplication.java
@@ -0,0 +1,13 @@
+package kz.runtime.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeinollaD240701SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeinollaD240701SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/runtime/spring/ZeinollaD240701SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.runtime.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeinollaD240701SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
